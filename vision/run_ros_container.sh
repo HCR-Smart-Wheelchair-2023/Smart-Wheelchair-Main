c0639d4f7d08c036dbcf9743d3dd90bfe67a8db9
Max Wickham <maxwickhammonro@gmail.com>
2023-02-15 15:36:47 +0000

    Update run_ros_container.sh

--- vision/run_ros_container.sh
+++ vision/run_ros_container.sh
@@ -5,4 +5,4 @@ source .env
 #xhost +
 
 #docker run --privileged --rm --gpus all --runtime nvidia -it --net=host -e DISPLAY=$DISPLAY --ipc=host -v /tmp/.X11-unix/:/tmp/.X11-unix/ -v /tmp/argus_socket:/tmp/argus_socket ${TAG_STAGE_1}
-docker run --privileged --rm --gpus all --runtime nvidia -it  ${TAG_STAGE_1}
+docker run --privileged --gpus all --runtime nvidia -it  ${TAG_STAGE_1}

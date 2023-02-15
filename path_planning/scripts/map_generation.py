491b83996d402d404b8918e2ab086d7e344653e0
Max Wickham <maxwickhammonro@gmail.com>
2023-02-15 16:32:26 +0000

    pgm

---
+++ path_planning/scripts/map_generation.py
@@ -0,0 +1,32 @@
+import numpy as np
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
+
+width = 1000
+height = 1000
+
+
+# add the coordinates of the points in the map 
+map_coordinates = [(100,100),(500,100),(500,900),(100,900)]
+
+
+# point = Point(0.5, 0.5)
+polygon = Polygon(map_coordinates)
+# print(polygon.contains(point))
+
+
+p_num = width * height
+arr = np.random.randint(0,255,p_num)
+
+with open('test.pgm', 'wb') as pgm_file:
+    pgmHeader = 'P5' + ' ' + str(width) + ' ' + str(height) + ' ' + str(255) +  '\n'
+    pgmHeader_byte = bytearray(pgmHeader,'utf-8')
+    pgm_file.write(pgmHeader_byte)
+
+    img = np.reshape(arr,(height,width))
+    for y in range(height):
+        print(y)
+        # bnd = list(img[j,:])
+        bnd = [255 if polygon.contains(Point(x, y)) else 0 for x in range(width)]
+        # print(bnd)
+        pgm_file.write(bytearray(bnd)) # for 8-bit data only

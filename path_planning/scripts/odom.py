8b4a094f755cffc9b32f1d897eb672a638a8a663
Max Wickham <maxwickhammonro@gmail.com>
2023-02-22 16:48:51 +0000

    changes

--- path_planning/scripts/odom.py
+++ path_planning/scripts/odom.py
@@ -33,15 +33,15 @@ def odom_callback(msg):
     # prev_rotation = rotation
     br = tf.TransformBroadcaster()
     print('publishing transform')
-    br.sendTransform((msg.pose.pose.position.x + 10, msg.pose.pose.position.y + 10, msg.pose.pose.position.z),
+    msg.pose.pose.position.x += 10
+    msg.pose.pose.position.y += 10
+    br.sendTransform((msg.pose.pose.position.x, msg.pose.pose.position.y, msg.pose.pose.position.z),
                      (msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w),
                      rospy.Time.now(),
                      "base_link",
                      "odom")
     if not sim:
         # print('sending')
-        msg.pose.pose.position.x += 10
-        msg.pose.pose.position.y += 10
         msg.header.frame_id = 'odom'
         pub.publish(msg)
 

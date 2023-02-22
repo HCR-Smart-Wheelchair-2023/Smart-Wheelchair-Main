0d25ef7b2229507908f21f63f808be38b8394811
Max Wickham <maxwickhammonro@gmail.com>
2023-02-22 14:51:59 +0000

    stuf

--- path_planning/scripts/point_cloud_transform.py
+++ path_planning/scripts/point_cloud_transform.py
@@ -32,7 +32,8 @@ def callback(point_cloud_msg: PointCloud2):
         transformed_cloud = do_transform_cloud(point_cloud_msg, trans)
     else:
         transformed_cloud = point_cloud_msg
-    print('Sending Message')
+    print(transformed_cloud.header.frame_id)
+    # print('Sending Message')
     transformed_cloud.header.frame_id = map_frame
     pub.publish(transformed_cloud)
 

63e12c00e9f3640fa3f008b42610afe3598a6547
timeo-schmidt <timeo.schmidt@gmx.de>
2023-02-24 18:29:06 +0000

    integrated vision files from timeos repo into the main repo

--- vision/ros_entrypoint.sh
+++ vision/ros_entrypoint.sh
@@ -2,9 +2,10 @@
 set -e
 
 # setup ros environment
-source "/opt/ros/$ROS_DISTRO/setup.bash" 
+source "/opt/ros/noetic/setup.bash" 
+source "/opt/ros_ws/devel/setup.bash" 
 
 # roslaunch zed_wrapper zed2i.launch
-roslaunch zed_display_rviz display_zed2i.launch    
+# roslaunch zed_display_rviz display_zed2i.launch    
 
 exec "$@" # pass all arguments to the entrypoint

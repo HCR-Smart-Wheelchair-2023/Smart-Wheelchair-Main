34cfc92ad8b81100f1c189367e6656c30a296bd2
bjs3118 <59967644+bjs3118@users.noreply.github.com>
2023-02-27 22:32:12 +0000

    fixed topic

--- social_predictions/src/social_predictions.py
+++ social_predictions/src/social_predictions.py
@@ -66,7 +66,7 @@ def social_predict(costmap, object_pos, velocity, t):
 class MapProcessor:
     def __init__(self):
         self.map_sub = rospy.Subscriber('/map', OccupancyGrid, self.map_callback_map, queue_size=1)
-        self.update_sub = rospy.Subscriber('/tracked_people', ObjectsStamped, self.map_callback_update, queue_size=1)
+        self.update_sub = rospy.Subscriber('/zed2i/zed_node/obj_det/objects', ObjectsStamped, self.map_callback_update, queue_size=1)
         self.map_pub = rospy.Publisher('/adj_map', OccupancyGrid, queue_size=10)
         self.latest_map = None
 

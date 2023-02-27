ade2c51852ab713b808702be77c56749de2d979d
bjs3118 <bjs3118@ic.ac.uk>
2023-02-27 15:00:27 +0000

    people msg now uses Odometry msg

--- zed_dummy/src/dummy_detector.py
+++ zed_dummy/src/dummy_detector.py
@@ -3,8 +3,8 @@ import rospy
 import random
 
 from std_msgs.msg import String
-from people_msg.msg import People
-from zed_interfaces.msg import ObjectsStamped, Object
+from people_msg.msg import People, Person
+# from zed_interfaces.msg import ObjectsStamped, Object
 
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Point
@@ -13,13 +13,13 @@ from geometry_msgs.msg import Point
 
 rospy.init_node('zed_dummy')
 
-pub = rospy.Publisher('/tracked_people', ObjectsStamped, queue_size=10)
+pub = rospy.Publisher('/tracked_people', People, queue_size=10)
 rate = rospy.Rate(0.1)
 
 
 while not rospy.is_shutdown():
 
-    people_msg = ObjectsStamped()
+    people_msg = People()
 
     object_pos1 = Point(1.0, 2.0, 0.0)
     velocity1 = Point(random.uniform(-2, 2), random.uniform(-2, 2), 0.0)
@@ -31,12 +31,12 @@ while not rospy.is_shutdown():
     objs = [[object_pos1, velocity1], [object_pos2, velocity2], [object_pos3, velocity3]]
 
     for obj in range(1, 3):
-        odom_msg = Object()
+        odom_msg = Person()
         # Fill in the data for the odom_msg object
-        odom_msg.position = [random.uniform(-5, 5), random.uniform(-5, 5), 0.0]
-        odom_msg.velocity = [random.uniform(-2, 2), random.uniform(-2, 2), 0.0]
+        odom_msg.odom.pose.pose.position = Point(random.uniform(-5, 5), random.uniform(-5, 5), 0.0)
+        odom_msg.odom.twist.twist.linear = Point(random.uniform(-2, 2), random.uniform(-2, 2), 0.0)
 
-        people_msg.objects.append(odom_msg)
+        people_msg.person.append(odom_msg)
 
 
     pub.publish(people_msg)

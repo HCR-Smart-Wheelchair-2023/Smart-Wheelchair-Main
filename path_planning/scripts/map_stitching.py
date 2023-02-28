d401bfe5fa371a094ad6a3d54a4256be89f39a1f
Max Wickham <maxwickhammonro@gmail.com>
2023-02-28 18:36:27 +0000

    change

--- path_planning/scripts/map_stitching.py
+++ path_planning/scripts/map_stitching.py
@@ -38,7 +38,7 @@ class MapStitcher:
         y_min = int(self.dynamic_map.info.origin.position.y - int(self.dynamic_map.info.width/2))
         y_max = int(self.dynamic_map.info.origin.position.y + ceil(self.dynamic_map.info.width/2))
         rospy.loginfo(f'{x_min},{x_max},{y_min},{y_max},{self.dynamic_map.info.height}')
-        static_array[x_min:x_max,y_min:y_max] += map_array
+        static_array[(x_min+500):(x_max+500),(y_min+500):(y_max+500)] += map_array
         static_array = static_array.reshape(self.static_map.info.width*self.static_map.info.height)
         self.static_map.data = static_array
         self.pub.publish(self.static_map)

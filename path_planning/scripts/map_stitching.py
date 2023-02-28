38728af693c5dd23e3f89b01e069a566d4445eaf
Max Wickham <maxwickhammonro@gmail.com>
2023-02-28 18:23:59 +0000

    hcr

--- path_planning/scripts/map_stitching.py
+++ path_planning/scripts/map_stitching.py
@@ -2,6 +2,7 @@
 
 import rospy
 import numpy as np
+from math import ceil
 from nav_msgs.msg import OccupancyGrid
 
 class MapStitcher:
@@ -16,25 +17,38 @@ class MapStitcher:
         rospy.spin()
 
     def static_map_callback(self, map_data):
-        if self.dynamic_map is not None:
-            self.static_map_array = np.array(map_data.data)
-            self.static_map_array.reshape((map_data.info.height, map_data.info.width))
-            padding = max(int(map_data.info.height - self.dynamic_map.info.height), 0)
-            self.static_map_array = np.pad(self.static_map_array, pad_width=padding, mode='constant', constant_values=0)
-            self.static_map_array = self.static_map_array.reshape(
-                (self.static_map_array.shape[0]*self.static_map_array.shape[1])
-            )
+        self.static_map = map_data
+        self.static_array = np.array(self.static_map.data).reshape((self.static_map.info.width,self.static_map.info.height))
+
 
     def dynamic_map_callback(self, map_data):
         self.dynamic_map = map_data
-        if self.known_position and self.static_map_array is not None:
+        if self.known_position and self.static_map is not None:
             self.merge_maps()
-        self.pub.publish(self.dynamic_map)
+        # self.pub.publish(self.dynamic_map)
 
     def merge_maps(self):
         map_array = np.array(self.dynamic_map.data)
-        map_array = np.add(self.static_map_array, map_array)
-        self.dynamic_map.data = map_array
+        # map_array = np.add(self.static_map_array, map_array)
+        # self.dynamic_map.data = map_array
+        map_array = map_array.reshape((self.dynamic_map.info.height, self.dynamic_map.info.width))
+        static_array = self.static_array
+        x_min = self.dynamic_map.info.center.origin.position.x - int(self.dynamic_map.info.height/2)
+        x_max = self.dynamic_map.info.center.origin.position.x - ceil(self.dynamic_map.info.height/2)
+        y_min = self.dynamic_map.info.center.origin.position.y - int(self.dynamic_map.info.width/2)
+        y_max = self.dynamic_map.info.center.origin.position.y - ceil(self.dynamic_map.info.width/2)
+        static_array[x_min:x_max,y_min:y_max] += map_array
+        static_array = static_array.reshape(self.static_map.info.width*self.static_map.info.height)
+        self.static_map.data = static_array
+        self.pub.publish(self.static_map)
+
+        # map_array = np.pad(map_array, pad_width=((int(self.dynamic_map))))
+        # self.static_map_array.reshape((map_data.info.height, map_data.info.width))
+        #     padding = max(int(map_data.info.height - self.dynamic_map.info.height), 0)
+        #     self.static_map_array = np.pad(self.static_map_array, pad_width=padding, mode='constant', constant_values=0)
+        #     self.static_map_array = self.static_map_array.reshape(
+        #         (self.static_map_array.shape[0]*self.static_map_array.shape[1])
+        #     )
 
 if __name__ == '__main__':
     MapStitcher()

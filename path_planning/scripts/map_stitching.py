ec510e7f3ec2881bb0eef5351d680b3b3012b6fc
Max Wickham <maxwickhammonro@gmail.com>
2023-02-28 18:38:30 +0000

    map

--- path_planning/scripts/map_stitching.py
+++ path_planning/scripts/map_stitching.py
@@ -34,11 +34,11 @@ class MapStitcher:
         map_array = map_array.reshape((self.dynamic_map.info.height, self.dynamic_map.info.width))
         static_array = self.static_array
         x_min = int(self.dynamic_map.info.origin.position.x - int(self.dynamic_map.info.height/2))
-        x_max = int(self.dynamic_map.info.origin.position.x + int(self.dynamic_map.info.height/2))
+        x_max = int(self.dynamic_map.info.origin.position.x + ceil(self.dynamic_map.info.height/2))
         y_min = int(self.dynamic_map.info.origin.position.y - int(self.dynamic_map.info.width/2))
-        y_max = int(self.dynamic_map.info.origin.position.y + int(self.dynamic_map.info.width/2))
+        y_max = int(self.dynamic_map.info.origin.position.y + ceil(self.dynamic_map.info.width/2))
         rospy.loginfo(f'{x_min},{x_max},{y_min},{y_max},{self.dynamic_map.info.height}')
-        static_array[(x_min+500):(x_max+500),(y_min+500):(y_max+500)] += map_array
+        static_array[(x_min+499):(x_max+500),(y_min+499):(y_max+500)] += map_array
         static_array = static_array.reshape(self.static_map.info.width*self.static_map.info.height)
         self.static_map.data = static_array
         self.pub.publish(self.static_map)

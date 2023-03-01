4b306a9a0626e7ea00bcc9677f88d1be579b965e
lg519 <59980519+lg519@users.noreply.github.com>
2023-03-01 13:35:18 +0000

    Create aruco_pose.py

---
+++ path_planning/scripts/aruco_pose.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import rospy
+from geometry_msgs.msg import PoseStamped
+from std_srvs.srv import SetBool, SetBoolRequest
+
+
+class ArUcoCameraController:
+    def __init__(self):
+        self.aruco_pose_sub = rospy.Subscriber(
+            "/aruco_single/pose", PoseStamped, self.aruco_pose_callback
+        )
+        self.set_pose_service = rospy.ServiceProxy("/set_pose", SetBool)
+
+    def aruco_pose_callback(self, pose_stamped):
+        # Extract the position and orientation of the ArUco marker
+        aruco_position = pose_stamped.pose.position
+        aruco_orientation = pose_stamped.pose.orientation
+
+        # Use the ArUco marker's position and orientation to update the camera pose
+        success = self.set_pose_service(
+            X=aruco_position.x,
+            Y=aruco_position.y,
+            Z=aruco_position.z,
+            R=aruco_orientation.x,
+            P=aruco_orientation.y,
+            Y=aruco_orientation.z,
+        )
+
+        if success:
+            rospy.loginfo("Camera pose updated")
+        else:
+            rospy.logerr("Failed to update camera pose")
+
+
+if __name__ == "__main__":
+    rospy.init_node("aruco_camera_controller")
+    aruco_camera_controller = ArUcoCameraController()
+    rospy.spin()

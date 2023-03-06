e1fcc8dd178fefb543ea07cb53b1641e5fbaf6f7
lg519 <lg519@ic.ac.uk>
2023-03-06 18:40:01 +0000

    changed form /map to map

--- aruco_detection/src/my_pose_publisher.py
+++ aruco_detection/src/my_pose_publisher.py
@@ -27,7 +27,8 @@ if __name__ == "__main__":
 
             # create a PoseStamped message for the marker
             marker_pose = PoseStamped()
-            marker_pose.header.frame_id = "/map"
+            marker_pose.header.frame_id = "map"
+            marker_pose.header.stamp = rospy.Time.now()
             marker_pose.pose.position.x = 0.87
             marker_pose.pose.position.y = 7.74
             marker_pose.pose.position.z = 1.42

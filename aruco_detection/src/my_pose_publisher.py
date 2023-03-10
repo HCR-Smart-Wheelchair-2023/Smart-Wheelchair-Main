2de193156c8900f40a222b682339649bebc4bbe3
lg519 <lg519@ic.ac.uk>
2023-03-10 17:10:32 +0000

    conversion from quaternion to euler angles

--- aruco_detection/src/my_pose_publisher.py
+++ aruco_detection/src/my_pose_publisher.py
@@ -4,6 +4,7 @@ import rospy
 import tf
 from geometry_msgs.msg import PoseStamped, TransformStamped
 import time
+import math
 
 if __name__ == "__main__":
     rospy.init_node("Marker_pose_publisher_node")
@@ -20,6 +21,10 @@ if __name__ == "__main__":
     print("test1")
     time.sleep(2)
 
+    # angle is in radians
+    angle = math.pi / 2
+    quaternion = tf.transformations.quaternion_from_euler(0, 0, angle)
+    print(quaternion)
     while not rospy.is_shutdown():
         try:
             print("test2")
@@ -32,10 +37,10 @@ if __name__ == "__main__":
             marker_pose.pose.position.x = 0.87
             marker_pose.pose.position.y = 7.74
             marker_pose.pose.position.z = 1.42
-            marker_pose.pose.orientation.x = 0.0
-            marker_pose.pose.orientation.y = 0.0
-            marker_pose.pose.orientation.z = 1.57
-            marker_pose.pose.orientation.w = 0.0
+            marker_pose.pose.orientation.x = quaternion[0]
+            marker_pose.pose.orientation.y = quaternion[1]
+            marker_pose.pose.orientation.z = quaternion[2]
+            marker_pose.pose.orientation.w = quaternion[3]
             # Publish the message
             pub.publish(marker_pose)
             print(marker_pose)

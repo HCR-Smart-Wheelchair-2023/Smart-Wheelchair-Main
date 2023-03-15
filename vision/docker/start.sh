a0f400352e32acfa52934008a8a4f869a1b9de09
timeo-schmidt <timeo.schmidt@gmx.de>
2023-03-15 19:43:24 +0100

    2

--- vision/docker/start.sh
+++ vision/docker/start.sh
@@ -7,5 +7,6 @@ export ROS_MASTER_URI=http://192.168.50.101:11311
 #roslaunch zed_wrapper zed2i.launch
 #apt-get install --fix-missing -y ros-noetic-aruco-ros
 #./aruco.sh &
-roslaunch zed_multicamera_example_with_rtabmap zed_multi_cam.launch
+# roslaunch zed_multicamera_example_with_rtabmap zed_multi_cam.launch
+roslaunch zed_rtabmap_example zed_rtabmap.launch
 # roslaunch zed_rtab_two_cameras zed_rtabmap.launch

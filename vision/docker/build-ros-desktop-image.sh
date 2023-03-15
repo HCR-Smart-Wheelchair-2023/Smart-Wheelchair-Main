e253d38d6d94d47607971943027dfcc81d94da67
timeo-schmidt <timeo.schmidt@gmx.de>
2023-03-15 19:42:21 +0100

    1

--- vision/docker/build-ros-desktop-image.sh
+++ vision/docker/build-ros-desktop-image.sh
@@ -9,6 +9,7 @@ docker build --build-arg ZED_SDK_MAJOR=${ZED_SDK_MAJOR} \
 --build-arg L4T_MAJOR_VERSION=${L4T_MAJOR_VERSION} \
 --build-arg L4T_MINOR_VERSION=${L4T_MINOR_VERSION} \
 --build-arg ROS_DISTRO_ARG=${ROS_DISTRO_ARG} \
+--no-cache \
 -t "${TAG_STAGE_1}" -f "${DOCKERFILE_STAGE_1}" .
 
 echo "Building '${TAG_STAGE_2}'"

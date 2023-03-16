e471acac73a08116b4deb390a117b071f7dfe10d
bjs3118 <59967644+bjs3118@users.noreply.github.com>
2023-03-16 19:37:51 +0000

    soc nav testing

--- social_predictions/src/social_predictions.py
+++ social_predictions/src/social_predictions.py
@@ -222,13 +222,15 @@ class MapProcessor:
                 pos, vals = draw_Gaussian(self.latest_map, person.odom.pose.pose.position, theta, distribution_scale_factor, gaus_sep)
                 # print("stat:",vals)
                 for (i, pos) in enumerate(pos):
-                    adj_map.data[pos] = vals[i] #min(100, vals[i]+ adj_map.data[pos])
+                    if vals[i] != 0:
+                        adj_map.data[pos] = vals[i] #min(100, vals[i]+ adj_map.data[pos])
 
             else:
                 pos, vals = social_predict_Gaussian(self.latest_map, person.odom.pose.pose.position, person.odom.twist.twist.linear, distribution_scale_factor, t)
                 # print("mov:", vals)
                 for (i, pos) in enumerate(pos):
-                    adj_map.data[pos] = vals[i] #min(100, vals[i]+ adj_map.data[pos])
+                    if vals[i] != 0:
+                        adj_map.data[pos] = vals[i] #min(100, vals[i]+ adj_map.data[pos])
 
 
                 # adjusted_cells += social_predict(self.latest_map, person.odom.pose.pose.position, person.odom.twist.twist.linear, 5)

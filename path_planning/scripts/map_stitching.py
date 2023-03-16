e6094581aad31407cba0c75463a08a8908ad88ef
Max Wickham <maxwickhammonro@gmail.com>
2023-03-16 13:41:10 +0000

    change

--- path_planning/scripts/map_stitching.py
+++ path_planning/scripts/map_stitching.py
@@ -86,8 +86,10 @@ class MapStitcher:
         y_offset = (y_max-y_min) - map_array.shape[1]
 
         static_array = np.vectorize(lambda x : 255 - x)(static_array)
+        map_array = np.vectorize(lambda x : 255 if x > 0 else 0)(map_array)
         map_array = np.vectorize(lambda x : 255 - x)(map_array)
         static_array[(x_min+500+x_offset):(x_max+500),(y_min+500+y_offset):(y_max+500)] += map_array
+
         static_array = static_array.reshape(self.static_map.info.width*self.static_map.info.height)
         static_array = np.vectorize(lambda x : 255 if x < 200 else 0)(static_array)
         self.static_map.data = np.clip(static_array,0,255)

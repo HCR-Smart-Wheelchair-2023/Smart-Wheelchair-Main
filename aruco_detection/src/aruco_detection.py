8b38a0dc4098209c0a066b5d195b81e139a0532d
lg519 <lg519@ic.ac.uk>
2023-03-16 20:13:47 +0000

    ind

--- aruco_detection/src/aruco_detection.py
+++ aruco_detection/src/aruco_detection.py
@@ -106,9 +106,16 @@ class ArUcoCameraController:
         )
 
         # swap angles
-        
-            aruco_orientation_euler[0], aruco_orientation_euler[1], aruco_orientation_euler[2] = aruco_orientation_euler[2], aruco_orientation_euler[0],aruco_orientation_euler[1]
 
+        (
+            aruco_orientation_euler[0],
+            aruco_orientation_euler[1],
+            aruco_orientation_euler[2],
+        ) = (
+            aruco_orientation_euler[2],
+            aruco_orientation_euler[0],
+            aruco_orientation_euler[1],
+        )
 
         aruco_orientation_euler_degrees = [
             aruco_orientation_euler[0] * 180 / math.pi,

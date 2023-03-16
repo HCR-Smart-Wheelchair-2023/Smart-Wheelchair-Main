1c1ef07ad40e29d48d60e3ad81631e480265e79e
lg519 <lg519@ic.ac.uk>
2023-03-16 18:41:34 +0000

    moving average fileter implemented

--- aruco_detection/src/aruco_detection.py
+++ aruco_detection/src/aruco_detection.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import rospy
-from geometry_msgs.msg import PoseStamped, TransformStamped
+from geometry_msgs.msg import PoseStamped, TransformStamped, Pose
 from std_msgs.msg import String
 from aruco_msgs.msg import MarkerArray, Marker
 from zed_interfaces.srv import set_pose, set_poseRequest
@@ -15,16 +15,6 @@ Set_Pose = set_pose()
 
 class ArUcoCameraController:
     def __init__(self):
-        # self.aruco_pose_sub = rospy.Subscriber(
-        #     "/aruco_single/pose", PoseStamped, self.aruco_pose_callback
-        # )
-
-        # self.aruco_transform_sub = rospy.Subscriber(
-        #     "/aruco_single/transform", TransformStamped, self.aruco_transform_callback
-        # )
-
-        print("IN ARUCOCAMERACONTROLLER")
-
         self.aruco_markerArray_sub = rospy.Subscriber(
             "/aruco_marker_publisher/markers",
             MarkerArray,
@@ -51,16 +41,9 @@ class ArUcoCameraController:
         self.marker_transform.transform.rotation.z = quaternion[2]
         self.marker_transform.transform.rotation.w = quaternion[3]
 
-        # create a PoseStamped message for the marker
-        # self.marker_pose = PoseStamped()
-        # self.marker_pose.header.frame_id = "/camera_link"
-        # self.marker_pose.pose.position.x = 0.87
-        # self.marker_pose.pose.position.y = 7.74
-        # self.marker_pose.pose.position.z = 1.42
-        # self.marker_pose.pose.orientation.x = 0.0
-        # self.marker_pose.pose.orientation.y = 0.0
-        # self.marker_pose.pose.orientation.z = 0.0
-        # self.marker_pose.pose.orientation.w = 1.0
+        # initialize buffer for moving average filter
+        self.buffer = []
+        self.buffer_size = 10
 
         # # publish the marker pose
         # self.pub.publish(self.marker_pose)
@@ -82,85 +65,26 @@ class ArUcoCameraController:
         except rospy.ServiceException as e:
             print("service not working yet")
 
-    # def aruco_transform_callback(self, transform_stamped):
-    #     # Extract the position and orientation of the ArUco marker
-
-    #     aruco_position = transform_stamped.transform.translation
-    #     print(f"aruco position: {aruco_position}")
-    #     aruco_orientation = transform_stamped.transform.rotation
-    #     print(f"aruco orientation: {aruco_orientation}")
-
-    #     transform_camera_aruco = tf.transformations.concatenate_matrices(
-    #         tf.transformations.translation_matrix(
-    #             [aruco_position.x, aruco_position.y, aruco_position.z]
-    #         ),
-    #         tf.transformations.quaternion_matrix(
-    #             [
-    #                 aruco_orientation.x,
-    #                 aruco_orientation.y,
-    #                 aruco_orientation.z,
-    #                 aruco_orientation.w,
-    #             ]
-    #         ),
-    #     )
-    #     print(f"transform_camera_aruco: {transform_camera_aruco}")
-    #     transform_aruco_camera = tf.transformations.inverse_matrix(
-    #         transform_camera_aruco
-    #     )
-
-    #     transfrom_world_aruco = tf.transformations.concatenate_matrices(
-    #         tf.transformations.translation_matrix(
-    #             [
-    #                 self.marker_transform.transform.translation.x,
-    #                 self.marker_transform.transform.translation.y,
-    #                 self.marker_transform.transform.translation.z,
-    #             ]
-    #         ),
-    #         tf.transformations.quaternion_matrix(
-    #             [
-    #                 self.marker_transform.transform.rotation.x,
-    #                 self.marker_transform.transform.rotation.y,
-    #                 self.marker_transform.transform.rotation.z,
-    #                 self.marker_transform.transform.rotation.w,
-    #             ]
-    #         ),
-    #     )
-    #     print(f"transfrom_world_aruco: {transfrom_world_aruco}")
-
-    #     transform_world_camera = tf.transformations.concatenate_matrices(
-    #         transfrom_world_aruco, transform_aruco_camera
-    #     )
-
-    #     camera_position = tf.transformations.translation_from_matrix(
-    #         transform_world_camera
-    #     )
-
-    #     camera_orientation = tf.transformations.euler_from_matrix(
-    #         transform_world_camera
-    #     )
-
-    #     print(
-    #         f"camera position: {camera_position[0], camera_position[1], camera_position[2]}"
-    #     )
-    #     print(
-    #         f"camera orientation: {camera_orientation[0], camera_orientation[1], camera_orientation[2]}"
-    #     )
-
-    #     self.set_zedPose(
-    #         camera_position[0],
-    #         camera_position[1],
-    #         camera_position[2],
-    #         camera_orientation[0],
-    #         camera_orientation[1],
-    #         camera_orientation[2],
-    #     )
-
     def aruco_markerArray_callback(self, markerArray):
         # print("markerArray: ", markerArray)
         print("markerArray id of first marker: ", markerArray.markers[0].id)
 
         aruco_position = markerArray.markers[0].pose.pose.position
         print(f"aruco position: {aruco_position}")
+
+        # moving average filter
+        self.buffer.append(aruco_position)
+        if len(self.buffer) > self.buffer_size:
+            self.buffer.pop(0)
+
+            if len(self.buffer) == self.buffer_size:
+                average_pose = self.calculate_average_pose()
+                if self.is_pose_noise(aruco_position, average_pose):
+                    rospy.loginfo("Pose is too far from average, discarding...")
+                    return
+                else:
+                    aruco_position = average_pose
+
         aruco_orientation = markerArray.markers[0].pose.pose.orientation
         aruco_orientation_euler = tf.transformations.euler_from_quaternion(
             [
@@ -242,6 +166,29 @@ class ArUcoCameraController:
             camera_orientation[1],
             camera_orientation[2],
         )
+
+    def calculate_average_pose(self):
+        x_sum = 0.0
+        y_sum = 0.0
+        z_sum = 0.0
+        for pose in self.buffer:
+            x_sum += pose.position.x
+            y_sum += pose.position.y
+            z_sum += pose.position.z
+        average_pose = Pose()
+        average_pose.position.x = x_sum / self.buffer_size
+        average_pose.position.y = y_sum / self.buffer_size
+        average_pose.position.z = z_sum / self.buffer_size
+        return average_pose
+
+    def is_pose_noise(self, current_pose, average_pose, threshold=0.1):
+        if abs(current_pose.position.x - average_pose.position.x) > threshold:
+            return True
+        if abs(current_pose.position.y - average_pose.position.y) > threshold:
+            return True
+        if abs(current_pose.position.z - average_pose.position.z) > threshold:
+            return True
+        return False
 
 
 if __name__ == "__main__":

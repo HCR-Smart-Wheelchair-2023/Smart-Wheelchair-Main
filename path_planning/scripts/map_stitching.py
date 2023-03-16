72c584b8717e6f854579d3df975e2cfb8d773a6b
Max Wickham <maxwickhammonro@gmail.com>
2023-03-16 14:03:09 +0000

    errors

--- path_planning/scripts/map_stitching.py
+++ path_planning/scripts/map_stitching.py
@@ -65,7 +65,7 @@ class MapStitcher:
         static_array[(x_min+500+x_offset):(x_max+500),(y_min+500+y_offset):(y_max+500)] += map_array
 
         static_array = static_array.reshape(self.static_map.info.width*self.static_map.info.height)
-        # static_array = np.vectorize(lambda x : 255 if x < 200 else 0)(static_array)
+        static_array = np.vectorize(lambda x : 255 if x < 200 else 0)(static_array)
         self.static_map.data = [int(x) for x in np.clip(static_array,0,255)]
         self.pub.publish(self.static_map)
         rospy.loginfo(f'published')

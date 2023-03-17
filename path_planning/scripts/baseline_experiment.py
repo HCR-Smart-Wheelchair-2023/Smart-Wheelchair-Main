7a96969b6229bd4f03c37457f30068258fbace14
matilde piccoli <59872205+mp2419@users.noreply.github.com>
2023-03-17 19:03:21 +0000

    baseline

---
+++ path_planning/scripts/baseline_experiment.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python3
+
+import rospy
+from geometry_msgs.msg import Twist
+import time 
+import sys
+
+class HC():
+    def __init__(self):
+        rospy.init_node('HC')
+        #instantiate subscriber and publisher 
+        self.pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+
+    def start(self, scale=1):
+
+        data = Twist()
+        data.linear.x = 1*scale
+        self.pub.publish(data)
+
+        time.sleep(3)
+
+        data.linear.x = 0*scale
+        self.pub.publish(data) 
+
+        time.sleep(5) 
+        
+        data.linear.x = 1*scale
+        self.pub.publish(data)
+        time.sleep(5)
+        
+        data.angular.z = 1*scale
+        self.pub.publish(data)
+        time.sleep(2)
+        
+        data.linear.x = 0*scale
+        self.pub.publish(data) 
+
+scale = float(sys.argv[1])
+
+hc = HC()
+hc.start(scale)

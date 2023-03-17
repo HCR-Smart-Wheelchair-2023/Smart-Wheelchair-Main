24383970e6fd16c864b363da6de9603ea8362364
sdoif <59924600+sdoif@users.noreply.github.com>
2023-03-17 16:26:59 +0000

    Clean up and file paths

--- user_interface/scripts/get_goal.py
+++ user_interface/scripts/get_goal.py
@@ -1,24 +1,31 @@
 #! /usr/bin/env python3
 
 import rospy
-
 from std_msgs.msg import String
+
+import sys 
+
+file = '/root/ros_ws/src/user_interface/scripts/goal.txt'
 
 class GoalPublish():
     def __init__(self):
+
+        with open(file, "w") as f:
+            goal = f.write('')
+
         rospy.init_node('goal_publisher')
         #instantiate publisher 
         self.pub = rospy.Publisher('goal_dest', String, queue_size=10)
+        
         
     def start(self):
         rr = rospy.Rate(3)
         while not rospy.is_shutdown():
             # TODO: update the way we read file paths using os paths
-            with open("./goal.txt", "r") as f:
+            with open(file, "r") as f:
                 goal = f.read()
                 self.pub.publish(goal)
             rr.sleep()
-        # TODO: once shutdown, clear the file -- will probably need a try-except
         
 
 goal = GoalPublish()

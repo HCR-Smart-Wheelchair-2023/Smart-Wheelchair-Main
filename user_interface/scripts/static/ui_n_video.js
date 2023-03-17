3da49b402e9f39003d0eb64c1bc675b9b313f76f
yr419 <60004524+yr419@users.noreply.github.com>
2023-03-17 17:55:37 +0000

    updated ui

--- user_interface/scripts/static/ui_n_video.js
+++ user_interface/scripts/static/ui_n_video.js
@@ -70,24 +70,44 @@ bathroomBut.addEventListener('click',publishMessage('bathroom'));
     recognition.onresult = function(event) {
       const transcript = event.results[event.results.length - 1][0].transcript;
       
-      if (transcript.includes("hello") && (transcript.includes("door"))) {
-        inputField.value = "go to door";
+      // if (transcript.includes("hello") && (transcript.includes("door"))) {
+      //   inputField.value = "go to door";
+      //   door();}
+      // else if (transcript.includes("hello") && (transcript.includes("kitchen"))){
+      //   inputField.value = "go to kitchen";
+      //   kitchen();
+      // }
+      // else if (transcript.includes("hello") && (transcript.includes("table"))){
+      //   inputField.value = "go to table";
+      //   table();
+      // }
+      // else if (transcript.includes("hello") && (transcript.includes("bathroom"))){
+      //   inputField.value = "go to bathroom";
+      //   bathroom();
+      // }
+      // else {
+      //   inputField.value = "error";
+      // }
+      if (transcript.includes("hello")&&transcript.includes("chair")&&transcript.includes("door")){
+        inputField.value = "door";
         door();}
-      else if (transcript.includes("hello") && (transcript.includes("kitchen"))){
-        inputField.value = "go to kitchen";
-        kitchen();
-      }
-      else if (transcript.includes("hello") && (transcript.includes("table"))){
-        inputField.value = "go to table";
-        table();
-      }
-      else if (transcript.includes("hello") && (transcript.includes("bathroom"))){
-        inputField.value = "go to bathroom";
-        bathroom();
-      }
-      else {
-        inputField.value = "error";
-      }
+
+       else if (transcript.includes("hello")&&transcript.includes("chair") && (transcript.includes("kitchen"))){
+         inputField.value = "go to kitchen";
+         kitchen();
+       }
+       else if (transcript.includes("hello")&&transcript.includes("chair") && (transcript.includes("table"))){
+         inputField.value = "go to table";
+         table();
+       }
+       else if (transcript.includes("hello")&&transcript.includes("chair") && (transcript.includes("bathroom"))){
+         inputField.value = "go to bathroom";
+         bathroom();
+       }
+       else {
+         inputField.value = "error";
+       }
+
     };
     
     recognition.onend = function() {
@@ -99,8 +119,53 @@ bathroomBut.addEventListener('click',publishMessage('bathroom'));
           recognition.stop();
         });
   }
+//---------------------------------------------------code for toggle-------------------------------------------------------
+function toggleColor() {
 
+  const button = document.getElementById('toggle');
+  const currentColor = button.style.backgroundColor;
+  const newColor = currentColor === 'white' ? 'rgb(29, 31, 161)' : 'white';
+  button.style.backgroundColor = newColor;
+  const currentColor_border = button.style.borderColor;
+  const newColor_border = currentColor_border === 'yellow' ? 'rgb(0, 5, 77)' : 'yellow';
+  button.style.borderColor = newColor_border;
 
+  
+  const button1 = document.getElementById('door');
+  button1.style.backgroundColor = newColor;
+  button1.style.borderColor = newColor_border;
+
+  const button2 = document.getElementById('bathroom');
+  button2.style.backgroundColor = newColor;
+  button2.style.borderColor = newColor_border;
+
+  const button3 = document.getElementById('kitchen');
+  button3.style.backgroundColor = newColor;
+  button3.style.borderColor = newColor_border;
+
+  const button4 = document.getElementById('table');
+  button4.style.backgroundColor = newColor;
+  button4.style.borderColor = newColor_border;
+
+  const button5 = document.getElementById('voice_recog');
+  button5.style.backgroundColor = newColor;
+  button5.style.borderColor = newColor_border;
+
+  const button6 = document.getElementById('video');
+  button6.style.backgroundColor = newColor;
+  button6.style.borderColor = newColor_border;
+
+  const body = document.querySelector("body");
+  const currentColor_back = body.style.backgroundColor;
+  const newColor_back = currentColor_back === 'black' ? 'rgb(173, 216, 230)' : 'black';
+	body.style.backgroundColor = newColor_back;
+
+  const title = document.querySelector("h1");
+  const currentColor_h1 = h1.style.colour;
+  const newColor_h1 = currentColor_h1 === 'black' ? 'rgb(173, 216, 230)' : 'black';
+	h1.style.Color = newColor_h1;
+
+}
   // -----------------------------------------------code to use camera---------------------------------------------------
   // Get the video element
   let video = null

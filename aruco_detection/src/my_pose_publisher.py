bc2821171f12de229d85a3992b48fdf054b06c05
lg519 <lg519@ic.ac.uk>
2023-03-17 17:21:24 +0000

    test

--- aruco_detection/src/my_pose_publisher.py
+++ aruco_detection/src/my_pose_publisher.py
@@ -19,9 +19,9 @@ if __name__ == "__main__":
     rate = rospy.Rate(10.0)  # Publish at 10 Hz
 
     # angle is in radians
-    angle_x = 0 * math.pi / 180
+    angle_x = 90 * math.pi / 180
     angle_y = 0 * math.pi / 180
-    angle_z = -90 * math.pi / 180
+    angle_z = 0 * math.pi / 180
 
     quaternion = tf.transformations.quaternion_from_euler(angle_x, angle_y, angle_z)
     print(quaternion)

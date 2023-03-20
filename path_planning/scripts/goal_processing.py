49597e37452a2803959bb560e1ef535db5d67ff2
Max Wickham <maxwickhammonro@gmail.com>
2023-03-20 18:58:33 +0000

    ed changes

--- path_planning/scripts/goal_processing.py
+++ path_planning/scripts/goal_processing.py
@@ -14,32 +14,37 @@ class GoalController:
         rospy.init_node('goal_processing')
         self.goal_label_sub  = rospy.Subscriber('/goal_dest', String, self.receive_goal_label_sub)
         self.client = actionlib.SimpleActionClient('move_base',MoveBaseAction)
-        self.client.wait_for_server()
         self.GOALS = {
-        'kitchen' : [258, 196],
-        'bathroom' : [500, 500],
-        'table' : [326,410],
-        'door' : [100, 150]
+            'kitchen' : [2.58, 1.96],
+            'bathroom' : [5.00, 5.00],
+            'table' : [3.26,4.10],
+            'door' : [1.00, 1.50]
         }
+        self.client.wait_for_server()
+        rospy.spin()
 
     def receive_marker(self, marker):
         ...
 
     def receive_goal_label_sub(self, goal_label):
+        if goal_label.data not in self.GOALS:
+            return
         goal = MoveBaseGoal()
         goal.target_pose.header.frame_id = "map"
         goal.target_pose.header.stamp = rospy.Time.now()
+        rospy.loginfo(f'{self.__dict__}')
         goal.target_pose.pose.position.x = self.GOALS[goal_label.data][0]
         goal.target_pose.pose.position.y = self.GOALS[goal_label.data][1]
         self.client.send_goal(goal)
         rospy.loginfo('Recieved move to'+goal_label.data+'command')
+        rospy.loginfo(f'Goal {goal}')
         wait = self.client.wait_for_result()
         rospy.loginfo(f'{wait}')
 
-    def start(self):
-        rr = rospy.Rate(3)
-        while not rospy.is_shutdown():
-            rr.sleep()
+    # def start(self):
+    #     rr = rospy.Rate(3)
+    #     while not rospy.is_shutdown():
+    #         rr.sleep()
 
 gc = GoalController()
-gc.start()
+# gc.start()

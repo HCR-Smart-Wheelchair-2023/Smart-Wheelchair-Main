7fb39e534f3c1a07dcd522d6dbbbdf5376aadeea
sdoif <59924600+sdoif@users.noreply.github.com>
2023-03-23 18:39:43 +0000

    gp

--- path_planning/scripts/goal_processing.py
+++ path_planning/scripts/goal_processing.py
@@ -5,6 +5,7 @@ import rospy
 from std_msgs.msg import String
 import actionlib
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+from geometry_msgs.msgs import PoseStamped
 # sub to the topic that subscribes to marker positions
 
 
@@ -19,19 +20,19 @@ class GoalController:
         }
         rospy.init_node('goal_processing')
         self.goal_label_sub  = rospy.Subscriber('/goal_dest', String, self.receive_goal_label_sub)
-        # self.pub = rospy.Publisher('/move_base/simple/goal', MoveBaseGoal, queue_size=10)
-        self.client = actionlib.SimpleActionClient('/move_base',MoveBaseAction)
-        self.client.wait_for_server()
+        self.pub = rospy.Publisher('/move_base_simple/goal', MoveBaseGoal, queue_size=10)
+        #self.client = actionlib.SimpleActionClient('/move_base',MoveBaseAction)
+        #self.client.wait_for_server()
         rospy.spin()
 
     def receive_marker(self, marker):
         ...
 
     def receive_goal_label_sub(self, goal_label):
-        rospy.loginfo(f'Goal {goal}')
+
         if goal_label.data not in self.GOALS:
             return
-        goal = MoveBaseGoal()
+        goal = PoseStamped()
         goal.target_pose.header.frame_id = "map"
         goal.target_pose.header.stamp = rospy.Time.now()
         #rospy.loginfo(f'{self.__dict__}')
@@ -40,11 +41,12 @@ class GoalController:
 
         goal.target_pose.pose.orientation.z = 0.342
         goal.target_pose.pose.orientation.w = 0.940
-        self.client.send_goal(goal)
+        self.pub.publish(goal.target_pose)
+        #self.client.send_goal(goal)
         #rospy.loginfo('Recieved move to'+goal_label.data+'command')
-        rospy.loginfo(f'Goal {goal}')
-        wait = self.client.wait_for_result()
-        rospy.loginfo(f'{wait}')
+        rospy.loginfo(f'Goal {goal.target_pose}')
+        #wait = self.client.wait_for_result()
+        #rospy.loginfo(f'{wait}')
 
     # def start(self):
     #     rr = rospy.Rate(3)
